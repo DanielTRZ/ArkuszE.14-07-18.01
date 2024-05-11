b652f69ea9f0e549dca3eaf36ffa6aa6ff9f6ef0
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-05-11 12:56:42 +0200

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -7,7 +7,7 @@ var oceny=0;
     
 if(isNaN(uczen1)||uczen1==""||isNaN(uczen2)||uczen2==""||isNaN(uczen3)||uczen3=="")
 {
-          alert("Wpisz poprawne dane");
+alert("Wpisz poprawne dane");
          // obliczenia.innerHTML ="wpisz poprawne dane";  
         }
     else
